53c33300ad4e196ccc6104287461b27a58a8b22b
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-23 20:45:31 +0530

    added route handling function

--- book-managment-mysql/packages/configs/app.go
+++ book-managment-mysql/packages/configs/app.go
@@ -1,10 +1,1 @@
 package configs
-
-import (
-	"github.com/gorilla/mux"
-	// "Golang_backend/book-managment-mysql/packages/controllers"
-)
-
-var RegisterBookStoreRoutes = func(router *mux.Router) {
-
-}
