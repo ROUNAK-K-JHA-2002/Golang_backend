dbeb0657604dd0fe82dc96a18af03012871bc2c7
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-23 18:34:17 +0530

    Updated go.mod for reading local packages

--- book-managment-mysql/packages/configs/app.go
+++ book-managment-mysql/packages/configs/app.go
@@ -1,1 +1,10 @@
 package configs
+
+import (
+	"github.com/gorilla/mux"
+	// "Golang_backend/book-managment-mysql/packages/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module example.com/m/v2
+module Golang_backend
 
 go 1.20
 
